76357edb99670eac12cc2f4cafbfb0a60a1f7bb3
Vitor Hugo <vitor.hugo.cq@live.com>
2021-12-28 22:56:53 -0300

    construção inicial tela de provas

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -3,6 +3,9 @@ import { Title } from '@angular/platform-browser';
 
 import { NotificationService } from '../../core/services/notification.service';
 import { NGXLogger } from 'ngx-logger';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-list',
@@ -10,6 +13,14 @@ import { NGXLogger } from 'ngx-logger';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  askSelected = 'option0';
+  subcjetSelected = 'option0';
+  themeSelected = 'option0';
+  levelSelected = 'option0';
+  typeSelected = 'option0';
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
   constructor(
     private logger: NGXLogger,
@@ -20,5 +31,15 @@ export class UserListComponent implements OnInit {
   ngOnInit() {
     this.titleService.setTitle('angular-material-template - Users');
     this.logger.log('Users loaded');
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
