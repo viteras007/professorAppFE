74251e618c2feefa6e9ffe91935f0f77e54a8f0b
viteras007 <viteras__@hotmail.com>
2022-01-03 21:47:04 -0300

    add login

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyA1Gp4AOJDye5iZ3oZJ8_6t2NyFilIcIRM',
+  authDomain: 'professorappbe.firebaseapp.com',
+  projectId: 'professorappbe',
+  storageBucket: 'professorappbe.appspot.com',
+  messagingSenderId: '234120862120',
+  appId: '1:234120862120:web:8d65ea1189446b5b26e59d',
+  measurementId: 'G-DZ7XRPT9DH'
+});
 
 if (environment.production) {
   enableProdMode();
@@ -12,3 +23,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
